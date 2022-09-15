5500a7babd5335635642b28a4f01959e84c0db35
Pozsár Zsolt <pozsarzs@karak>
2022-09-15 14:30:41 +0200

    220915

--- source/commonproc.pp
+++ source/commonproc.pp
@@ -13,18 +13,19 @@
 // FOR A PARTICULAR PURPOSE.
 
 {
- Mode  Operation  Output data    Input data
- -----------------------------------------------------------------------------
- Mode0  Stand-by  -      -
- Mode1  BUce    N/P;Pwr      BUce
- Mode2  BBcb    N/P;Pwr      BUcb
- Mode3  Ieb0    N/P;Pwr      Ieb0
- Mode4  Icb0    N/P;Pwr      Icb0
- Mode5  Ice0    N/P;Pwr      Ice0
- Mode6  Charact.  N/P;Pwr;Uce;5xIb  20xIb,Ube; 5x20xIc,Uce
- Mode7  Charact.  N/P;Pwr;Uce;5xIb  20xIb,Ube; 5x20xIc,Uce
- Mode8  Selection  N/P;Pwr;?
+ Mode	Operation	Input data		Output data
+ ---------------------------------------------------------------------
+ Mode0	Stand-by	-			-
+ Mode1	BUce		N/P;Pwr			BUce
+ Mode2	BBcb		N/P;Pwr			BUcb
+ Mode3	Ieb0		N/P;Pwr			Ieb0
+ Mode4	Icb0		N/P;Pwr			Icb0
+ Mode5	Ice0		N/P;Pwr			Ice0
+ Mode6	Charact.	N/P;Pwr;Uce;Ubem	20xIb,Ube
+ Mode7	Charact.	N/P;Pwr;Uce;5xIb	20xIb,Ube; 5x20xIc,Uce
+ Mode8	Selection	N/P;Pwr;?
 }
+
 unit commonproc;
 
 {$MODE OBJFPC}{$H-}
